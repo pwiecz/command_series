9628d8029fd555bc3150ded4b31360e2a9dd9b9f
Piotr Wieczorek <p@wie.cz>
2020-09-26 15:43:08 +0200

    A tool for splitting war.sid file into separate files

---
+++ splitter/splitter.go
@@ -0,0 +1,37 @@
+package main
+
+import "bufio"
+import "io/ioutil"
+import "fmt"
+import "os"
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	buf, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	started := false
+	progIdx := 0
+	var startPosition int
+	var currentProgram []byte
+	for i, b := range buf {
+		if !started && b != 0x9b {
+			continue
+		}
+		if !started {
+			if buf[i+1] >= '0' && buf[i+1] <= '9' {
+				continue
+			}
+			startPosition = i
+			started = true
+		}
+		currentProgram = append(currentProgram, b)
+		if b == 0x0c {
+			fmt.Printf("%d\n", i+1 - len(currentProgram) - startPosition)
+			ioutil.WriteFile(fmt.Sprintf("prg_%d.sid", progIdx), currentProgram, 0644)
+			currentProgram = nil
+			progIdx += 1
+		}
+	}
+}
