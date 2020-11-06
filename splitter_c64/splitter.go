823353420f5ca161c10b54b5bf4029e3dde78f3e
Piotr Wieczorek <p@wie.cz>
2020-11-06 21:01:37 +0100

    Add a splitter tool for c64 version of .sid file
    It's almost identical to the Atari version

---
+++ splitter_c64/splitter.go
@@ -0,0 +1,31 @@
+package main
+
+import "bufio"
+import "io/ioutil"
+import "fmt"
+import "os"
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	buf, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	started := false
+	progIdx := 0
+	var startPosition int
+	var currentProgram []byte
+	for i, b := range buf {
+		if !started && b != 0x18 {
+			continue
+		}
+		started = true
+		currentProgram = append(currentProgram, b)
+		if b == 0x0c {
+			fmt.Printf("%d\n", i+1-len(currentProgram)-startPosition)
+			ioutil.WriteFile(fmt.Sprintf("prg_%d.sid", progIdx), currentProgram, 0644)
+			currentProgram = nil
+			progIdx++
+		}
+	}
+}
