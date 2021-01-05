bebae01d63f14c5f02fb84e665a94e004bb00e48
Piotr Wieczorek <p@wie.cz>
2021-01-05 11:07:41 +0100

    Add a forgotten file

---
+++ demo/save_load.go
@@ -0,0 +1,33 @@
+package main
+
+import "io/ioutil"
+import "os"
+import "path/filepath"
+import "strings"
+
+func saveDir(scenario string) (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(homeDir, ".command_series", "saves", scenario), nil
+}
+
+func listSaveFiles(scenario string) []string {
+	saveDir, err := saveDir(scenario)
+	if err != nil {
+		return nil
+	}
+	files, err := ioutil.ReadDir(saveDir)
+	if err != nil {
+		return nil
+	}
+	var saveFiles []string
+	for _, fileInfo := range files {
+		if fileInfo.IsDir() || !strings.HasSuffix(fileInfo.Name(), ".sav") {
+			continue
+		}
+		saveFiles = append(saveFiles, fileInfo.Name())
+	}
+	return saveFiles
+}
